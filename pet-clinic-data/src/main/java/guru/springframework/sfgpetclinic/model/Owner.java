9a1313b78f4cf5b2135769406d93596ee67d35b4
sourabhsmf <sourabhs@mindfiresolutions.com>
2020-02-24 11:12:47 +0530

    Added address,city,telephone and set of pets to owner model.Enhanced save method to handle pet types.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -1,5 +1,6 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -7,13 +8,41 @@ import java.util.Set;
  */
 public class Owner extends Person {
 
+    private String address;
+    private String telephone;
+    private String city;
     private Integer age;
-    private Set<Pet> pets;
-    
+    private Set<Pet> pets = new HashSet<>();
+
     public Owner(Integer age) {
         this.age = age;
     }
-    public void setAge(Integer age){
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setAge(Integer age) {
         this.age = age;
     }
     public Integer getAge(){
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerServiceMap.java
@@ -4,11 +4,21 @@ import java.util.Set;
 import org.springframework.stereotype.Service;
 
 import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.model.Pet;
 import guru.springframework.sfgpetclinic.services.OwnerService;
+import guru.springframework.sfgpetclinic.services.PetService;
+import guru.springframework.sfgpetclinic.services.PetTypeService;
 
 @Service
 public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService{
 
+    private final PetTypeService petTypeService;
+    private final PetService petService;
+
+    public OwnerServiceMap(PetTypeService petTypeService , PetService petService){
+        this.petTypeService = petTypeService;
+        this.petService = petService;
+    }
     @Override
     public Set<Owner> findAll() {
         return super.findAll();
@@ -36,6 +46,21 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
     }
     @Override
     public Owner save(Owner owner) {
+        if(owner != null && owner.getPets() != null){
+            owner.getPets().forEach(pet -> {
+                if(pet.getPetType() != null) {
+                    if(pet.getPetType().getId() == null){
+                        pet.setPetType(petTypeService.save(pet.getPetType()));
+                    } 
+                }else{
+                    throw new RuntimeException("Pet type is required");
+                }
+                if(pet.getId() == null){
+                    Pet savedPet = petService.save(pet);
+                    pet.setId(savedPet.getId());
+                } 
+            });
+        }    
         return super.save(owner);
     }
        

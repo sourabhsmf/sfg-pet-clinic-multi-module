3251600a394cb47b4ec23938edcc6c9454f50ffa
sourabhsmf <sourabhs@mindfiresolutions.com>
2020-03-12 11:00:26 +0530

    Testing failures of test. #34

--- pet-clinic-data/src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/OwnerSDJpaServiceTest.java
+++ pet-clinic-data/src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/OwnerSDJpaServiceTest.java
@@ -62,7 +62,7 @@ class OwnerSDJpaServiceTest {
     @Test
     void findById() {
         when(ownerRepository.findById(anyLong())).thenReturn(Optional.of(owner));
-        assertNotNull(ownerSDJpaService.findById(1L));
+        assertNull(ownerSDJpaService.findById(1L));
     }
 
     @Test
